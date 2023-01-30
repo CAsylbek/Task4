5fec3191bfc70964e6a81f187cf89443138bf351
Asylbek <119057661+CAsylbek@users.noreply.github.com>
2023-01-30 09:28:09 +0600

    task6

--- src/main/java/com/task4/task4/XmlValidator.java
+++ src/main/java/com/task4/task4/XmlValidator.java
@@ -1,10 +1,5 @@
 package com.task4.task4;
 
-import com.task4.task4.model.DTO.TextDocumentDto;
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Marshaller;
-import jakarta.xml.bind.Unmarshaller;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -16,32 +11,21 @@ import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
-import java.io.StringWriter;
 
 public class XmlValidator {
 
     private String xsdPath;
-    private JAXBContext jaxbContext;
-    private Marshaller marshaller;
-    private Unmarshaller unmarshaller;
+    private Validator validator;
 
-
-    public XmlValidator(String xsdPath) throws JAXBException {
+    public XmlValidator(String xsdPath) throws SAXException {
         this.xsdPath = xsdPath;
-        jaxbContext = JAXBContext.newInstance(TextDocumentDto.class);
-        marshaller = jaxbContext.createMarshaller();
-        unmarshaller = jaxbContext.createUnmarshaller();
-    }
-
-    private Validator initValidator() throws SAXException {
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         Source schemaFile = new StreamSource(new File(xsdPath));
         Schema schema = factory.newSchema(schemaFile);
-        return schema.newValidator();
+        validator = schema.newValidator();
     }
 
-    public boolean isValid(String xml) throws IOException, SAXException {
-        Validator validator = initValidator();
+    public boolean isValid(String xml) throws IOException {
         try {
             validator.validate(new StreamSource(new StringReader(xml)));
             return true;
